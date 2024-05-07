0ddfa79365408f8bf7186fb45d1a1cd43f8727d2
Ruphasa <rfrizqifauzan@gmail.com>
2024-05-07 20:05:57 +0700

    coba ke tomi

--- penyewa.php
+++ penyewa.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <script src="js/bootstrap.bundle.min.js" ></script>
-    <link rel="stylesheet" href="font/css/font-awesome.min.css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
     <title>Document</title>
 </head>
 <body>
