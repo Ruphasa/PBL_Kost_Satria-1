c5f74be79c90afe5453cd4c60df698523dfbaf5d
Tomi Martino Affandi <tomimartino10@gmail.com>
2024-05-10 16:26:36 +0700

    ADD : pop up

--- main.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once  __ROOT__ .'parent/parent.php';
-require_once  __ROOT__ .'penyewa/penyewa.php';
-
-use parent\parent;
-use penyewa\penyewa;
-
-?>
--- transaksi.php
+++ transaksi.php
@@ -59,10 +59,26 @@
                                 <i class="fa fa-pencil"></i>
                                 Ubah
                             </a>
-                            <a href="proses.php?hapus2=1" type="button" class="btn btn-danger btn-sm">
-                                <i class="fa fa-trash"></i>
-                                Hapus
-                            </a>
+                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Hapus
+                            </button>
+                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Hapus Data</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ingin menghapus data ini?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <a href="proses.php?hapus2=1" type="button" class="btn btn-primary">Save changes</a>
+                                </div>
+                                </div>
+                        </div>
+                        </div>
                         </td>
                         </tr>
                         <?php endforeach; ?>
